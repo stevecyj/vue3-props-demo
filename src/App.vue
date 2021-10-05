799cd8ceaeb5bd17f15af0a95355eb626ae6ee90
stevecyj <ycsteven.tsao@gmail.com>
2021-10-05 17:25:18 +0800

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import PropsTest from '@/components/PropsTest.vue';
+import { ref } from 'vue';
+export default {
+  components: {
+    PropsTest,
+  },
+  setup() {
+    const data = ref('hello moto');
+    return { data };
+  },
+};
+</script>
+
+<template>
+  <PropsTest :msg="data" />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PropsTest.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: ['msg'],
+  setup(props) {
+    return { props };
+  },
+};
+</script>
+
+<template>
+  <h1>JOJO {{ props.msg }}</h1>
+</template>
+
+<style>
+h1 {
+  color: aqua;
+  font-size: 50px;
+}
+</style>
